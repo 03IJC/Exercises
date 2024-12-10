42dac099f133ed3f88805f6b2b6fd8dba1e6df14
03IJC <cook.isaac.2003@gmail.com>
2024-12-09 21:14:20 -0800

    Selection Sort Added and Completed

---
+++ Sorting/README.md
@@ -0,0 +1,9 @@
+# Description
+Multiple different sorting algorithms
+
+
+# SelectionSort
+This sorting algorithm basically breaks an array into 2 parts, a sorted part and an unsorted part (It doesn't actually). 
+To the left of the current element is sorted; the current element and what is to the right of it is unsorted.
+Selection sort sorts by looking for the minimum (or maximum) in the unsorted part of the array then it swaps it with current element.
+This algorithm has a Big-Oh of O(n^2) in all cases.
---
+++ Sorting/src/SelectionSort.java
@@ -0,0 +1,106 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        //Initialize int array
+        int[] arr = {1, 4, 2, 4, 6, 2, 9, 1};
+
+        //Print out unsorted array
+        System.out.print("Array to sort: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
+
+        //Call selection sort method
+        int[] sortedArr = selectionSort(arr);
+        //int[] sortedArr = selectionSortPrinted(arr);
+
+        //Print out sorted Array
+        System.out.print("Sorted Array: ");
+        for (int i = 0; i < sortedArr.length; i++) {
+            System.out.print(sortedArr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Sorts an int array using selection sort (Low to high).
+     * @param arr
+     */
+    public static int[] selectionSort(int[] arr) {
+        //Iterate through entire array
+        for (int i = 0; i < arr.length; i++) {
+            //Initialize and set a minimum index to first element of unsorted part
+            int minIndex = i;
+
+            //Iterate through unsorted array
+            for (int j = i; j < arr.length; j++) {
+                //Set minimum index to current index if minimum index's value is greater than current index's
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+
+            //make the swap if the minimum index changed using a temp
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * Sorts an int array using selection sort (Low to high).
+     * Prints it broken up.
+     * @param arr
+     */
+    public static int[] selectionSortPrinted(int[] arr) {
+        //Iterate through entire array
+        for (int i = 0; i < arr.length; i++) {
+            //Print out currents
+            System.out.println("Current Index: " + i);
+            System.out.print("Current Array: ");
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+
+            //Print out what has been sorted
+            System.out.print("Sorted part:   ");
+            for (int j = 0; j < i; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+
+            //Print out what has not been sorted
+            System.out.print("Unsorted part: ");
+            for (int j = i; j < arr.length; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+
+            //Initialize and set a minimum index to first element of unsorted part
+            int minIndex = i;
+
+            //Iterate through unsorted array
+            for (int j = i; j < arr.length; j++) {
+                //Set minimum index to current index if minimum index's value is greater than current index's
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            System.out.println("Swapping index " + i + " with index " + minIndex);
+            System.out.println();
+            //make the swap if the minimum index changed using a temp
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+
+        return arr;
+    }
+}
